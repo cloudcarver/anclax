3fa0ede51b726bbbee80c0adb2d58429540364e8
cloudcarver <mikechesterwang@foxmail.com>
2025-05-12 13:39:38 +0800

    update version to v0.3.13

--- cmd/anchor/main.go
+++ cmd/anchor/main.go
@@ -12,7 +12,7 @@ var versionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "Show the version of anchor",
 	Action: func(c *cli.Context) error {
-		fmt.Println("v0.3.12")
+		fmt.Println("v0.3.13")
 		return nil
 	},
 }
